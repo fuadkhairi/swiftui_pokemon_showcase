e708a9f8a512c7df14cbcf065051bf416bc00829
fuadkhairi <fuadkhairihamid@gmail.com>
2023-08-04 21:52:54 +0800

    Initial Commit

---
+++ Poke_showcase/Poke_showcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poke_showcaseApp.swift
+//  Poke_showcase
+//
+//  Created by Fuad Khairi Hamid on 04/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Poke_showcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
